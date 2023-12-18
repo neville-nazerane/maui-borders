fca7e4c7a90f2b40a9707ac838f58b779025ba1e
Neville Nazerane <live.neville@live.com>
2023-12-18 04:36:34 -0700

    created a project to reproduce the border

---
+++ BorderShadow/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BorderShadow
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ BorderShadow/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BorderShadow
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
